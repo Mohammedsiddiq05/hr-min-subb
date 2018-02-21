66fe7e3cadf735869664c98f7e9d2b118273bc4f
Mohammedsiddiq05 <31438748+Mohammedsiddiq05@users.noreply.github.com>
2018-02-21 16:57:18 +0530

    Create hr:min sub.c

---
+++ hr:min sub.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int hour1,hour2;
+int min1,min2;
+int a,b;
+printf("\nenter the hour1: \tenter the min1:");
+scanf("\n%d \t%d",&hour1,&min1);
+printf("\nenter the hour2: \tenter the min2:");
+scanf("\n%d \t%d",&hour2,&min2);
+printf("\n%d \t%d",hour1,min1);
+printf("\n%d \t%d",hour2,min2);
+return 0;
+}
