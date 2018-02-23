4b750a632d77be1d7860a40d0c129ce7d4f30d0d
Mohammedsiddiq05 <31438748+Mohammedsiddiq05@users.noreply.github.com>
2018-02-23 16:09:46 +0530

    Update hr:min sub.c

--- hr:min sub.c
+++ hr:min sub.c
@@ -3,12 +3,15 @@ int main()
 {
 int hour1,hour2;
 int min1,min2;
-int a,b;
+int a,result;
 printf("\nenter the hour1: \tenter the min1:");
 scanf("\n%d \t%d",&hour1,&min1);
 printf("\nenter the hour2: \tenter the min2:");
 scanf("\n%d \t%d",&hour2,&min2);
 printf("\n%d \t%d",hour1,min1);
 printf("\n%d \t%d",hour2,min2);
+  a=hour1-hour2;
+  result=min1-min2;
+  printf("\nthe sub value is:%d %d",a,result);
 return 0;
 }
